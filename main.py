7593f33f5659a6128553796a1c705f2bc3479fdf
Vladyslav Ivanov <pedrobobrenko@gmail.com>
2025-08-23 13:25:02 +0200

    temporary allowed :5173 for frontend

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
-from strawberry.fastapi import GraphQLRouter
+from fastapi.middleware.cors import CORSMiddleware
 from schema import graphql_app
 
 app = FastAPI()
 
-app = FastAPI()
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(graphql_app, prefix="/graphql")
